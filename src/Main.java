f9130af1c7dab9eebe6437357b7ef36de605471e
Almat_Rakhmetolla <almat_rakhmetolla@epam.com>
2020-01-23 17:07:59 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Comparator<Person> nameComp = Comparator.comparing(Person::getName);
+//
+//        Comparator<Person> ageComp = Comparator.comparing(Person::getAge);
+//
+//        Set<Person> personSet = new TreeSet<>(nameComp);
+//        personSet.add(new Person("bbb", 15));
+//        personSet.add(new Person("ccc", 10));
+//        personSet.add(new Person("aaa", 20));
+//        personSet.add(new Person("ccc", 10));
+//
+//        personSet.forEach(System.out::println);
+
+        User user1 = new User("asd", "asd");
+        user1.setActive(true);
+
+        User user2 = new User("sdf", "sdf");
+        user2.setActive(true);
+
+        Predicate<User> isActive = x -> x.getActive();
+        System.out.println(isActive.test(user1));
+
+        BinaryOperator<User> compare = ((u1, u2) -> (u1.getFirstName().compareTo(u2.getFirstName()) >= 0) ? u2 : u1);
+        System.out.println(compare.apply(user1, user2));
+
+        UnaryOperator<User> deactivete = user -> {
+            user.setActive(false);
+            return user;
+        };
+
+        deactivete.apply(user1);
+        System.out.println(user1);
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,40 @@
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private Boolean isActive;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
+
+interface UserFactory{
+    User create(String firstName, String lastName);
+}
